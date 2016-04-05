4056f4edae8f89724e0b789bf96dbbb0edc9e9f6
tdongsi <dongsi.tuecuong@gmail.com>
2016-04-04 17:14:08 -0700

    Some properties for regular calendar.

--- calendars/calendars.py
+++ calendars/calendars.py
@@ -13,11 +13,79 @@ https://en.wikipedia.org/wiki/4%E2%80%934%E2%80%935_calendar
 @author: tdongsi
 """
 
+from datetime import date
+
 # Each fiscal year starts on August 1st.
 FISCAL_START = (8, 1)
 
 
 class RegularDate(object):
+
+    def __init__(self, mdate, today=None):
+        """ Initialize a date in regular calendar with the given datetime.date object.
+
+        :param mdate: the given datetime.date object.
+        :param today: default is the current date (today), if not provided.
+        :return:
+        """
+        self._date = mdate
+        if not today:
+            self._today = date.today()
+        else:
+            self._today = today
+
+        self.year_start = date(self._date.year, 1, 1)
+        self.year_end = date(self._date.year, 12, 31)
+
+    @property
+    def year(self):
+        """ Return the calendar year of the given date.
+
+        :return:
+        """
+        return self._date.year
+
+    @property
+    def year_start_date(self):
+        """ Start date of the calendar year containing this date instance.
+
+        :return:
+        """
+        return self.year_start
+
+    @property
+    def year_end_date(self):
+        """ End date of the calendar year containing this date instance.
+
+        :return:
+        """
+        return self.year_end
+
+    @property
+    def year_num_of_days(self):
+        """ Number of days in the calendar year containing this date instance.
+
+        :return: Number of days in the calendar year containing this date instance.
+        """
+        diff = self.year_end - self.year_start
+        return diff.days + 1
+
+    @property
+    def is_current_year(self):
+        """ Is this instance in the current calendar year, if today is as given?
+
+        :return: True/False
+        """
+        return True if (self._today.year == self.year) else False
+
+    @property
+    def is_previous_year(self):
+        """ Is the given date in the previous calendar year, if today is as given?
+
+        :return:
+        """
+        return True if (self._today.year - 1 == self.year) else False
+
     pass
 
 
---
+++ tests/test_regular.py
@@ -0,0 +1,88 @@
+"""
+Test classes and functions for regular calendar.
+Use pytest module as the main test framework.
+"""
+
+from datetime import date
+import unittest
+
+from calendars.calendars import RegularDate
+
+
+class IsCurrentYearTest(unittest.TestCase):
+    """
+    Test cases for is_current_year and is_previous_year properties
+    1. Run with today
+    2. Mock with some day: find current year.
+    """
+
+    def test_general(self):
+
+        today = RegularDate(date.today())
+        # today should be in current calendar year
+        self.assertEqual(today.is_current_year, True)
+        self.assertEqual(today.is_previous_year, False)
+
+    def test_year2018(self):
+        """Pretend today as mid year 2018.
+        """
+        today = RegularDate(date(2018, 6, 15))
+        self._year2018_test_cases(today)
+
+    def test_year2018_start(self):
+        """Pretend today as start date of year 2018.
+        """
+        today = date(2018, 1, 1)
+        self._year2018_test_cases(today)
+
+    def test_year2018_end(self):
+        """Pretend today as end date of year 2018.
+        """
+        today = date(2018, 12, 31)
+        self._year2018_test_cases(today)
+
+    def _year2018_test_cases(self, today):
+        """ Test instances in year 2018, depending on what today is.
+        """
+
+        input_date = RegularDate(date(2018, 1, 1), today)
+        self._verify_current_calendar_year(input_date, True, False)
+
+        input_date = RegularDate(date(2018, 12, 31), today)
+        self._verify_current_calendar_year(input_date, True, False)
+
+        input_date = RegularDate(date(2018, 6, 15), today)
+        self._verify_current_calendar_year(input_date, True, False)
+
+        input_date = RegularDate(date(2017, 1, 1), today)
+        self._verify_current_calendar_year(input_date, False, True)
+
+        input_date = RegularDate(date(2017, 12, 31), today)
+        self._verify_current_calendar_year(input_date, False, True)
+
+        input_date = RegularDate(date(2017, 6, 15), today)
+        self._verify_current_calendar_year(input_date, False, True)
+
+        input_date = RegularDate(date(2019, 1, 1), today)
+        self._verify_current_calendar_year(input_date)
+        input_date = RegularDate(date(2019, 12, 31), today)
+        self._verify_current_calendar_year(input_date, False, False)
+        input_date = RegularDate(date(2016, 1, 1), today)
+        self._verify_current_calendar_year(input_date, False, False)
+        input_date = RegularDate(date(2016, 12, 31), today)
+        self._verify_current_calendar_year(input_date, False, False)
+
+    def _verify_current_calendar_year(self, input_date, expected_current=False, expected_previous=False):
+        """
+        Verify the values of is_current_year and is_previous_year properties with expected output.
+        """
+        self.assertEqual(input_date.is_current_year, expected_current)
+        self.assertEqual(input_date.is_previous_year, expected_previous)
+
+
+class RegularDateTest(unittest.TestCase):
+    """
+    Test cases for other properties that are simple enough.
+    """
+
+    pass
